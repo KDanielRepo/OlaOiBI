187bbd24c902a0996e4631703fb05f32e104c946
Komputer <Daniel200825@wp.pl>
2020-03-03 13:47:22 +0100

    k

--- zad1/src/Main.java
+++ zad1/src/Main.java
@@ -7,13 +7,13 @@ import java.util.Scanner;
 public class Main {
     String tekst= "";
     String wynik="";
-    Integer liczba;
+    Integer liczba=4;
 
 public void met1 (){
+    wynik="";
     for (int i = 0; i < tekst.length() ; i++) {
        char rzut = tekst.charAt(i);
        int ile = rzut+liczba;
-        System.out.println(ile);
        if (ile >122){
             ile=97+(ile-123);
        }
@@ -30,10 +30,12 @@ public void siorbPlik(File f){
         met1();
         System.out.println(wynik);
         File file = new File("./zad1/src/kek");
-        FileWriter fileWriter = new FileWriter(file);
+        FileWriter fileWriter = new FileWriter(file,true);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
         bufferedWriter.write(wynik);
+        bufferedWriter.newLine();
         bufferedWriter.close();
+        if (!file.exists())
         file.createNewFile();
     }catch (Exception e){
         e.printStackTrace();
@@ -42,11 +44,15 @@ public void siorbPlik(File f){
 
     public static void main(String[] args) {
         Main main = new Main();
-        Scanner nazwa = new Scanner(System.in);
+        /*Scanner nazwa = new Scanner(System.in);
         main.tekst = nazwa.nextLine();
         main.liczba = nazwa.nextInt();
         main.met1();
-        System.out.println(main.wynik);
-        main.siorbPlik(new File("./zad1/src/kektest.txt"));
+        System.out.println(main.wynik);*/
+        main.siorbPlik(new File("./zad1/src/kektest1.txt"));
+        main.siorbPlik(new File("./zad1/src/kektest2.txt"));
+        main.siorbPlik(new File("./zad1/src/kektest3.txt"));
+        main.siorbPlik(new File("./zad1/src/kektest4.txt"));
+        main.siorbPlik(new File("./zad1/src/kektest5.txt"));
     }
 }
